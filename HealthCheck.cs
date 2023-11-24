22aac50c3ff5408f9538ca83f9b661d5b841a36c
varangian-core <victor.vladimir.mikhaltsevich@gmail.com>
2023-11-24 10:06:55 -0800

    Reassigning the gh user and adding quick healthCheck

--- HealthCheck.cs
+++ HealthCheck.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace DefaultNamespace
 {
     public class HealthCheck
     {
-        private const int ConnectionTimeout = 5000; 
+        private const int ConnectionTimeout = 2000; 
 
         public static async Task<bool> IsPortOpen(string host, int port)
         {
@@ -17,21 +20,43 @@ namespace DefaultNamespace
                     var task = client.ConnectAsync(host, port);
                     if (await Task.WhenAny(task, Task.Delay(ConnectionTimeout)) == task)
                     {
-                        // Connection successful
                         return true;
                     }
                     else
                     {
-                        // Connection timed out
                         return false;
                     }
                 }
                 catch
                 {
-                    // Connection failed
                     return false;
                 }
             }
+        }
+
+        public static async Task<bool> IsServiceResponding(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var response = await httpClient.GetAsync(url);
+                    return response.IsSuccessStatusCode;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static bool IsCPUUsageUnderThreshold(int threshold)
+        {
+            var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            cpuCounter.NextValue();
+            Task.Delay(1000).Wait(); 
+            var currentUsage = cpuCounter.NextValue();
+            return currentUsage < threshold;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DefaultNamespace;
+using System;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        bool isPort5000Open = await HealthCheck.IsPortOpen("localhost", 5000);
+        Console.WriteLine($"Port 5000 is {(isPort5000Open ? "open" : "closed")}.");
+
+        bool isPort7160Open = await HealthCheck.IsPortOpen("localhost", 7160);
+        Console.WriteLine($"Port 7160 is {(isPort7160Open ? "open" : "closed")}.");
+    }
+}
