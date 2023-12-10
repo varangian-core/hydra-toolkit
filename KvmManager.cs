475236c1cba6a009e8540a2718206561e77ceb9f
varangian-core <mikhaltsevichv@gmail.com>
2023-12-09 19:03:15 -0800

    Add SSH.NET package and KvmManager class
    The SSH.NET package has been added to the project to facilitate remote server interactions. A new class, KvmManager, has been created for managing KVM support checks, which are performed via SSH commands executed on the remote host.

---
+++ KvmManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace HydraToolkit;
+using System;
+using Renci.SshNet;
+using System.IO;
+using System.Security;
+
+public class KvmManager
+{
+    private SecureString password; 
+
+    public KvmManager(SecureString password)
+    {
+        this.password = password; 
+    }
+
+    private string ExecuteSshCommand(string remoteHost, string username, string command)
+    {
+        using (var sshClient = new SshClient(remoteHost, username, new NetworkCredential(string.Empty, password).Password)) 
+        {
+            sshClient.Connect();
+            var sshCommand = sshClient.RunCommand(command);
+            sshClient.Disconnect();
+
+            return sshCommand.Result;
+        }
+    }
+
+    public bool CheckKvmSupport(string remoteHost, string username)
+    {
+        var command = $"egrep -c '(vmx|svm)' /proc/cpuinfo";
+        var result = ExecuteSshCommand(remoteHost, username, command);
+
+        int numOfSupportedProcesses;
+        if(int.TryParse(result, out numOfSupportedProcesses))
+        {
+            return numOfSupportedProcesses > 0;
+        }
+        else
+        {
+            // handle improperly formatted response
+            throw new Exception("Could not parse the response from the remote host.");
+        }
